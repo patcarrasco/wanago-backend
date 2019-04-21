5b7a642d4e7e862eed6fc6a2d0ff3e39766e780b
github username <patcarrasco@outlook.com>
2019-04-20 20:55:19 -0400

    cors allow herokuapp

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,12 @@ module Backend
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # config cors....
+
+    allow do
+      origins 'https://wanago.herokuapp.com/'
+    end
+
   end
 end
