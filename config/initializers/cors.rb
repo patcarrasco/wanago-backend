9d66218920baba085af5126d75636f9e9f22eb73
github username <patcarrasco@outlook.com>
2019-04-21 16:28:20 -0400

    edit cors for heroku only

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'https://www.wanago.herokuapp.com, https://wanago.herokuapp.com'
 
     resource '*',
       headers: :any,
