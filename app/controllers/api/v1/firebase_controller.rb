9bf14d084a0d48cf944766cf56a0fef06d3be5b6
github username <patcarrasco@outlook.com>
2019-04-20 22:17:56 -0400

    update cors

--- app/controllers/api/v1/firebase_controller.rb
+++ app/controllers/api/v1/firebase_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::FirebaseController < ApplicationController
 
   def creds
+    puts Rails.application.credentials.firebase[:config]
     render json: {key: Rails.application.credentials.firebase[:config]}
   end
 
